15c882cbb5c0e3d6f8267d36d08dc919c1c90d53
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2025-09-27 18:26:53 +0900

    코루틴 헬퍼 업데이트

--- Coroutine/CoroutineHelper.cs
+++ Coroutine/CoroutineHelper.cs
@@ -10,6 +10,7 @@ namespace Jeomseon.Coroutine
         public static WaitForFixedUpdate WaitForFixedUpdate { get; } = new WaitForFixedUpdate();
 
         private static readonly Dictionary<float, WaitForSeconds> _waitForSecondsDic = new Dictionary<float, WaitForSeconds>();
+        private static readonly Dictionary<float, WaitForSecondsRealtime> _waitForSecondsRealtimeDic = new Dictionary<float, WaitForSecondsRealtime>();
 
         public static WaitForSeconds WaitForSeconds(float delayTime)
         {
@@ -17,6 +18,17 @@ namespace Jeomseon.Coroutine
             {
                 value = new WaitForSeconds(delayTime);
                 _waitForSecondsDic.Add(delayTime, value);
+            }
+
+            return value;
+        }
+
+        public static WaitForSecondsRealtime WaitForSecondsRealtime(float delayTime)
+        {
+            if (_waitForSecondsRealtimeDic.TryGetValue(delayTime, out WaitForSecondsRealtime value))
+            {
+                value = new WaitForSecondsRealtime(delayTime);
+                _waitForSecondsRealtimeDic.Add(delayTime, value);
             }
 
             return value;
