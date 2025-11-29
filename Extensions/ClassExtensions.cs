bd0fd9c4170d50c243d63b64049f34bb64c2ed80
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2025-11-29 17:41:46 +0900

    오타자 수정

--- Extensions/ClassExtensions.cs
+++ Extensions/ClassExtensions.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Jeomseon.Extensiosn
+namespace Jeomseon.Extensions
 {
     public static class ClassExtensions 
     {
